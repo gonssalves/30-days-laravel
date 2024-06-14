d1312ab0c2a77506a7beb529e4c375e5bd83fe6e
gonssalves <vgo1@aluno.ifal.edu.br>
2024-06-14 18:29:40 -0300

    feat: add new view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+     <h1>This view was created with the command <i>php artisan make:view about</i></h1>
+</div>
