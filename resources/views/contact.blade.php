61dfdb151b0b8042f6d12d036fdaff3c5f906462
gonssalves <vgo1@aluno.ifal.edu.br>
2024-06-14 23:32:01 -0300

    feat: add simple navegation menu

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,16 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-     <h1>Contact view created with artisan</h1>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1>Hello from the Contact Page.</h1>
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
+    <title>Home</title>
 </head>
 <body>
     <nav>
@@ -11,6 +11,6 @@
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
     </nav>
-    <h1>Hello from the About Page.</h1>
+    <h1>Hello from the Home Page.</h1>
 </body>
 </html>
